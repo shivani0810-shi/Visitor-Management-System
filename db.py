ce86c427752694186fa5d9a975f53ba7d06c47b4
Shivani K <shishiv0810@gmail.com>
2026-06-22 12:03:12 +0530

    Remove hardcoded credentials

--- db.py
+++ db.py
@@ -1,14 +1,12 @@
+import os
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="vms-database-shishiv0810-3e4a.e.aivencloud.com",
-    port=16021,
-    user="avnadmin",
-    password="AVNS_dNf7ZTVuxQ_5LX2GbJa",
-    database="defaultdb",
-    ssl_disabled=False
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 16021)),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor(dictionary=True)
-
-print("Connected Successfully!")
